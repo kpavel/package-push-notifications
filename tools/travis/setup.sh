79424635ff8c0ba1037310b5d97282dbc83634ce
Belinda Vennam <beemarie88@gmail.com>
2018-08-21 14:20:13 -0400

    inital commit with files

---
+++ tools/travis/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+UTILDIR="$ROOTDIR/../incubator-openwhisk-utilities"
+HOMEDIR="$SCRIPTDIR/../../../"
+
+# clone utilties repo. in order to run scanCode.py
+cd ${HOMEDIR}
+git clone --depth 1 https://github.com/apache/incubator-openwhisk-utilities.git
+
+# shallow clone OpenWhisk repo.
+git clone --depth 1 https://github.com/apache/incubator-openwhisk.git openwhisk
+
+# shallow clone deploy package repo.
+git clone --depth 1 https://github.com/apache/incubator-openwhisk-package-deploy
+
+cd openwhisk
+./tools/travis/setup.sh
---
+++ tools/travis/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+# Build script for Travis-CI.
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+ROOTDIR="$SCRIPTDIR/../.."
+WHISKDIR="$ROOTDIR/../openwhisk"
+
+export OPENWHISK_HOME=$WHISKDIR
+
+cd ${ROOTDIR}
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  TERM=dumb ./gradlew :tests:test
+else
+  TERM=dumb ./gradlew :tests:testWithoutCredentials
+fi
