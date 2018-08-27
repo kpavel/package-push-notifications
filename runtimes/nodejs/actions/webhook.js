6f0e1e7397bf2d1c97868ca9356316bb3d9ab7aa
Belinda Vennam <beemarie88@gmail.com>
2018-08-27 14:51:56 -0400

    update webhook.js

--- runtimes/nodejs/actions/webhook.js
+++ runtimes/nodejs/actions/webhook.js
@@ -23,81 +23,77 @@
  *  @param {string} events - list of the events the webhook should fire on
  *  @return {object} whisk async
  */
-var request = require('request');
+const request = require('request');
 
 function main(params) {
-
-    console.log("push trigger feed params: ", params);
-    var parsedName = parseQName(params.triggerName);
-    var trigger = parsedName.name;
-    var namespace = parsedName.namespace;
-    var endpoint = 'openwhisk.ng.bluemix.net';
-    // URL of the whisk system. The calls of push service will go here.
-    var whiskCallbackUrl = 'https://' + process.env.__OW_API_KEY + "@" + endpoint + '/api/v1/namespaces/' + namespace + '/triggers/' + trigger;
-    var appId = params.appGuid || params.appId;
-    var appSecret = params.appSecret;
-    // The URL to create the webhook on push service
-    var registrationEndpoint = 'https://mobile.ng.bluemix.net/imfpush/v1/apps/' + appId + '/webhooks';
-    var lifecycleEvent = (params.lifecycleEvent || 'CREATE').trim().toUpperCase();
-    if (lifecycleEvent === 'CREATE' || lifecycleEvent === 'UPDATE') {
-
-        var events = params.events;
-        var body = {
-            name:trigger,
-            url: whiskCallbackUrl,
-            eventTypes: events
-        };
-        var options = {
-            method: 'POST',
-            url: registrationEndpoint,
-            body: JSON.stringify(body),
-            headers: {
-                'appSecret': appSecret,
-                'Content-Type': 'application/json'
-            }
-        };
-        var promise = new Promise(function(resolve, reject) {
-            request(options, function (error, response, body) {
-                if (error) {
-                    reject(error);
-                }
-                resolve({response: body});
-            });
-        });
-
-        return promise;
-    } else if (lifecycleEvent === 'DELETE') {
-        var options = {
-            method: 'DELETE',
-            url: registrationEndpoint,
-            headers: {
-                'appSecret': appSecret
-            }
-        };
-        var promise = new Promise(function(resolve, reject) {
-            request(options, function (error, response, body) {
-                if (error) {
-                    reject(error);
-                }
-                resolve({response: body});
-            });
-        });
-
-        return promise;
-    }
+  console.log("push trigger feed params: ", params);
+  const parsedName = parseQName(params.triggerName);
+  const trigger = parsedName.name;
+  const namespace = parsedName.namespace;
+  const endpoint = 'openwhisk.ng.bluemix.net';
+  // URL of the whisk system. The calls of push service will go here.
+  const whiskCallbackUrl = `https://${process.env.__OW_API_KEY}@${endpoint}/api/v1/namespaces/${namespace}/triggers/${trigger}`;
+  const appId = params.appGuid || params.appId;
+  const { appSecret } = params;
+  // The URL to create the webhook on push service
+  const registrationEndpoint = `https://mobile.ng.bluemix.net/imfpush/v1/apps/${appId}/webhooks`;
+  const lifecycleEvent = (params.lifecycleEvent || 'CREATE').trim().toUpperCase();
+  if (lifecycleEvent === 'CREATE' || lifecycleEvent === 'UPDATE') {
+    const { events } = params;
+    const body = {
+      name: trigger,
+      url: whiskCallbackUrl,
+      eventTypes: events,
+    };
+    const options = {
+      method: 'POST',
+      url: registrationEndpoint,
+      body: JSON.stringify(body),
+      headers: {
+        appSecret,
+        'Content-Type': 'application/json',
+      },
+    };
+    const promise = new Promise(((resolve, reject) => {
+      request(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        resolve({ response: body });
+      });
+    }));
+    return promise;
+  } if (lifecycleEvent === 'DELETE') {
+    const options = {
+      method: 'DELETE',
+      url: registrationEndpoint,
+      headers: {
+        appSecret,
+      },
+    };
+    const promise = new Promise(((resolve, reject) => {
+      request(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        resolve({ response: body });
+      });
+    }));
+    return promise;
+  }
 }
 
 function parseQName(qname) {
-    var parsed = {};
-    var delimiter = '/';
-    var defaultNamespace = '_';
-    if (qname && qname.charAt(0) === delimiter) {
-        var parts = qname.split(delimiter);
-        parsed.namespace = parts[1];
-        parsed.name = parts.length > 2 ? parts.slice(2).join(delimiter) : '';
-    } else {
-        parsed.namespace = defaultNamespace;
-        parsed.name = qname;
-    }
-    return parsed;
+  const parsed = {};
+  const delimiter = '/';
+  const defaultNamespace = '_';
+  if (qname && qname.charAt(0) === delimiter) {
+    const parts = qname.split(delimiter);
+    parsed.namespace = parts[1];
+    parsed.name = parts.length > 2 ? parts.slice(2).join(delimiter) : '';
+  } else {
+    parsed.namespace = defaultNamespace;
+    parsed.name = qname;
+  }
+  return parsed;
 }
